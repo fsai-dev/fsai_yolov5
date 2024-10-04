521624da0e9bc6c783a99c9a70975debd9ab95ee
Michael Mohamed <michael@foundationstack.com>
2024-10-03 22:46:01 -0400

    update

---
+++ classify/fsai-custom-predict-from-folder.py
@@ -0,0 +1,97 @@
+import os
+import sys
+from pathlib import Path
+import cv2
+import shutil
+import torch
+import torch.nn.functional as F
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.augmentations import classify_transforms
+from utils.dataloaders import LoadImages
+from utils.general import (
+    Profile,
+    check_img_size,
+)
+from utils.torch_utils import select_device
+
+
+def main():
+    weights = "../best.pt"
+    data_yaml = "../best.yml"
+    device = "cpu"  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+    imgsz = (224, 224)
+
+    #  Load model
+    device = select_device(device)
+    model = DetectMultiBackend(
+        weights, device=device, dnn=False, data=data_yaml, fp16=False
+    )
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+    bs = 1  # batch_size
+    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
+
+    # Get all images from the directory called './crops'
+    directory = os.path.join(ROOT, "crops")
+
+    # Get all the images in the directory
+    all_images = os.listdir(directory)
+
+    # Get the image paths
+    img_paths = [os.path.join(directory, img) for img in all_images]
+
+    for img_path in img_paths:
+        print(f"Processing image: {img_path}")
+
+        # Dataloader
+        dataset = LoadImages(
+            path=img_path,
+            img_size=imgsz,
+            transforms=classify_transforms(imgsz[0]),
+            vid_stride=1,
+        )
+
+        dt = (Profile(), Profile(), Profile())
+        for path, im, im0s, vid_cap, s in dataset:
+            with dt[0]:
+                im = torch.Tensor(im).to(model.device)
+                im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+                if len(im.shape) == 3:
+                    im = im[None]  # expand for batch dim
+
+            # Inference
+            with dt[1]:
+                results = model(im)
+
+            # Post-process
+            with dt[2]:
+                pred = F.softmax(results, dim=1)  # probabilities
+
+            enum_pred = enumerate(pred)
+
+            _, prob = next(enum_pred)
+            prediction = prob.argsort(0, descending=True).tolist()[0]
+            prediction_class = names[prediction]
+
+            print(f"Prediction: {prediction_class}")
+
+            # If prediction_class = FALSE_POSITIVE then copy to the false_positive directory
+            if prediction_class == "FALSE_POSITIVE":
+                print("Copying to false_positive directory")
+                shutil.copy(img_path, os.path.join(ROOT, "false_positives"))
+
+            # If prediction_class = TRUE_POSITIVE then copy to the true_positive directory
+            if prediction_class == "TRUE_POSITIVE":
+                print("Copying to true_positive directory")
+                shutil.copy(img_path, os.path.join(ROOT, "true_positives"))
+
+
+if __name__ == "__main__":
+    main()
